c7b2d7b74cc89020dd348b3625979d1c96058e07
Abdur rahman <abdurrahman21266057.com>
2026-01-12 07:08:52 +0600

    Refactor register function to improve token handling and cookie settings

--- server/controllers/authcontrollers.js
+++ server/controllers/authcontrollers.js
@@ -16,7 +16,19 @@ export const register = async(async (req, res) => {
     const hashedpassword = await bcrypt.hash(password, 10);
     const user = new userModel({
       name,email,password:hashedpassword})
-      await user.save()
+    await user.save()
+    
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+    
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      samesite: process.env.NODE_ENV === 'poduction' ? 'none' : 'strict',
+      maxAge: 7*24*60*60*1000,
+    });
 
   } catch (error) {
     res.json({ success: false, message: error.message });
